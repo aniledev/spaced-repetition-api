57608362f7a9f22a2eb9e6664b0c21417bda14a9
aniledev <elinamcgill@gmail.com>
2021-03-18 04:30:33 -0400

    FEAT: update table values in seed file

--- seeds/seed.tables.sql
+++ seeds/seed.tables.sql
@@ -1,18 +1,19 @@
 BEGIN;
 
-TRUNCATE
+  TRUNCATE
   "word",
   "language",
   "user";
 
-INSERT INTO "user" ("id", "username", "name", "password")
-VALUES
-  (
-    1,
-    'admin',
-    'Elina McGill Admin',
-    -- password = "pass"
-    '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG'
+  INSERT INTO "user"
+    ("id", "username", "name", "password")
+  VALUES
+    (
+      1,
+      'admin',
+      'Elina McGill Admin',
+      -- password = "pass"
+      '$2a$10$fCWkaGbt7ZErxaxclioLteLUgg4Q3Rp09WW0s/wSLxDKYsaGYUpjG'
   );
 
 INSERT INTO "language" ("id", "name", "user_id")
@@ -30,12 +31,15 @@ VALUES
   (7, 1, 'chien', 'dog', 8),
   (8, 1, 'chat', 'cat', null);
 
-UPDATE "language" SET head = 1 WHERE id = 1;
+  UPDATE "language" SET head = 1 WHERE id = 1;
 
--- because we explicitly set the id fields
--- update the sequencer for future automatic id setting
-SELECT setval('word_id_seq', (SELECT MAX(id) from "word"));
-SELECT setval('language_id_seq', (SELECT MAX(id) from "language"));
-SELECT setval('user_id_seq', (SELECT MAX(id) from "user"));
+  -- because we explicitly set the id fields
+  -- update the sequencer for future automatic id setting
+  SELECT setval('word_id_seq', (SELECT MAX(id)
+    from "word"));
+  SELECT setval('language_id_seq', (SELECT MAX(id)
+    from "language"));
+  SELECT setval('user_id_seq', (SELECT MAX(id)
+    from "user"));
 
-COMMIT;
+  COMMIT;
